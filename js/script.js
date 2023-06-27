6033ae32b1fb29d54f33e94b2541e0fbf2f1665a
1huali <wanhuali2000@gmail.com>
2023-06-27 15:40:42 -0400

    testing flower obj img - not done

--- js/script.js
+++ js/script.js
@@ -8,11 +8,23 @@ author, and this description to match your project!
 
 "use strict";
 window.onload = (event) => {
-    
+    //desktop settings :
+    document.getElementById("about-popUpTab").style="display:none";
+    document.getElementById("about-sidebar").style="display:block";
+    document.getElementById("mainButton").style="display:none";
+
+
     // mapp object :
     let map = L.map('map').setView([51.505, -0.09], 13);
     map.removeControl(map.zoomControl);
     
+    //flower object DOESTN WORK :
+//     let imageUrl = 'assets/images/flora-test.png',
+//     imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
+    
+// L.imageOverlay(imageUrl, imageBounds).addTo(map);
+// console.log(L.imageOverlay);
+
     // tile-images
     let tile1 = document.getElementById("tileImg1");
     let tile2 = document.getElementById("tileImg2");
@@ -28,7 +40,6 @@ tileImgArray.push(tile4);
 L.TileLayer.Kitten = L.TileLayer.extend({
     getTileUrl: function(coords) {
         let i = Math.ceil( Math.random() * 3 );
-        console.log(tileImgArray[i].src);
         return tileImgArray[i].src;
     }
 });
@@ -45,5 +56,27 @@ let popup = L.popup()
     .setContent("I am a standalone popup.")
     .openOn(map);
 
+let aboutButton = document.getElementById("mainButton");
+
+aboutButton.addEventListener("click", function(){
+console.log("bibidibabidbidoubooo")
+});
+
+
+//mobile setups
+if (L.Browser.mobile) {
+    console.log("mobile version");
+    document.getElementById("about-sidebar").style="display:none";
+    document.getElementById("mainButton").style="display:block";
+
+    aboutButton.addEventListener("click", function(){
+    document.getElementById("about-popUpTab").style="display:block";
+    });
+
+    document.getElementById("about-popUpTab").addEventListener("click", function(){
+    document.getElementById("about-popUpTab").style="display:none";
+    });
+
+}
 
 }; //end windown on load
