6a7fca62e0c639131153410064a418cd4677ad40
1huali <wanhuali2000@gmail.com>
2023-07-04 15:46:30 -0400

    click function in obj works

--- js/Axis.js
+++ js/Axis.js
@@ -1,33 +1,22 @@
-class Axis {
+    //this object creates a png into a map object on a map location and that is interactive
 
+class Axis {
     constructor(map,img,minBound,maxBound){
         this.map=map
         this.img=img;
         console.log(this.img)
         this.minBound=minBound;
         this.maxBound=maxBound;
-
-        // this.this.n_latLng =  new L.latLng(lat,lng);
+        this.imgOverlay = L.imageOverlay(this.img,[this.minBound, this.maxBound], {interactive:true}).addTo(map);
         // this.hoverTitle="";
         // this.hoverTxt="";
         // this.n_latLng = new L.latLng(lat,lng);
-        
     }
 
-    //this object creates a png into a map object on a map location and that is interactive
-    display(map){
-        // this.point = this.map.latLngToLayerPoint(this.n_latLng);
-        // this.xPos = this.point.x;
-        // this.yPos = this.point.y;
-        L.imageOverlay(this.img,[this.minBound, this.maxBound], {interactive:true}).addTo(map);
-
+    onClick(img){
+        this.imgOverlay.on('click', function() {
+            console.log('I have been clicked ' + img);
+        });    
     }
 
-    onClick(){
-    this.img.on("click", function () {
-    console.log("Clicked on flower");
-    });
-    }
-
-    
 } //fin
--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = (event) => {
   document.getElementById("about-popUpTab").style.display = "none";
   document.getElementById("about-sidebar").style.display = "block";
 
-  // map object:
+           //MAP SETTING ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀ 
   let map = L.map("map").setView([51.505, -0.09], 13);
   map.removeControl(map.zoomControl);
 
@@ -15,6 +15,7 @@ window.onload = (event) => {
     tileImg.src = `assets/data_jaune-splitIG/${i}.png`;
     tileImgArray.push(tileImg);
   }
+           //TILES SETTING ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀ 
 
   L.TileLayer.Custom = L.TileLayer.extend({
     getTileUrl: function (coords) {
@@ -33,9 +34,7 @@ window.onload = (event) => {
 
   L.tileLayer.custom().addTo(map);
 
-// IMAGE OBJECT ON MAP:
-// let imgObj3 = L.imageOverlay("assets/images/flowerAxis3.png",[[51.520493, -0.046692], [51.520493+10, -0.046692+17]], {interactive:true}).addTo(map);
-// let imgObj2 = L.imageOverlay("assets/images/flowerAxis2.png",[[40.773941+10, -74.12544+17], [40.773941, -74.12544]], {interactive:true}).addTo(map);
+           //AXIS OBJECTS ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀  ❀ 
 let axisArray=[];
 let axisObj = new Axis(map,"assets/images/flowerAxis.png",[36.315125, -21.796875],[36.315125+10, -21.796875+17]);
 let axisObj2 = new Axis(map,"assets/images/flowerAxis2.png",[40.773941+10, -74.12544+17], [40.773941, -74.12544]);
@@ -46,27 +45,8 @@ axisArray.push(axisObj3);
 
 
 for (let i=0;i<axisArray.length;i++){
-  axisArray[i].display(map);
-  axisArray[i].onClick();
+  axisArray[i].onClick(axisArray[i].img);
 }
-
-
-// on click:
-// imgObj.addEventListener("click", function(){
-//   console.log("clicked on flowa")
-// });
-
-// imgObj.on("click", function () {
-//   console.log("Clicked on flower");
-// });
-
-// imgObj2.on("click", function () {
-//   console.log("Clicked on flower2");
-// });
-
-// imgObj3.on("click", function () {
-//   console.log("Clicked on flower3");
-// });
 
 //POP-UP SETTINGS
   let popup = L.popup();
